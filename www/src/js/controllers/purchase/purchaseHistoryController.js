4b9c0d335e85f0d8713ee98a20dc83a8e09776e7
huiwenhua <huiwenhua@idriverplus.com>
2017-09-30 14:43:09 +0800

    采购跟踪适配手机显示
    Change-Id: I316a8730c68d38fe820c74f4c93b9e66e6b96bd8

--- www/src/js/controllers/purchase/purchaseHistoryController.js
+++ www/src/js/controllers/purchase/purchaseHistoryController.js
@@ -3,6 +3,9 @@ mainStart
     .controller('purchaseHistoryController',['$scope','$rootScope','$localStorage',function($scope,$rootScope,$localStorage){
         //获取角色权限
         $scope.roles = $localStorage.roles;
+        //消息推送
+        $scope.sendMessage = $localStorage.sendMessage;
         //获取角色信息
         $scope.user = $localStorage.user;
+
     }]);
