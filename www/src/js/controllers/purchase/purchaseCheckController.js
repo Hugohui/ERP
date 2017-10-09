0153b8a76c81fee32a23cdb011a2bca84b369700
huiwenhua <huiwenhua@idriverplus.com>
2017-10-09 13:01:13 +0800

    采购审核模态框
    Change-Id: Ic53bf9a24e6e0a3d92f58d8aab86e67fd6dd99ee

--- www/src/js/controllers/purchase/purchaseCheckController.js
+++ www/src/js/controllers/purchase/purchaseCheckController.js
@@ -8,4 +8,53 @@ mainStart
         //获取角色信息
         $scope.user = $localStorage.user;
 
+        /*加载采购申请列表*/
+        /*$.ajax({
+            type:'POST',
+            url:'http://111.204.101.170:11115',
+            data:{
+                action:"getPurchaseListCheck",
+                params:{
+                    "userName":"张三",
+                    limit:"10",
+                    start:"0",
+                    page:"1",
+                    queryData:{
+                        startDate:"2017-10-11",
+                        endDate:"2017-10-11",
+                        status:"0",
+                        purchase_applicant_id:"CGSQ20170912001",
+                        queryApplicant:"张三"
+                    }
+                }
+            },
+            dataType: 'jsonp',
+            jsonp : "callback",
+            success:function(data){
+                $scope.purchaseList=data.resData.data;
+            }
+        })*/
+
+        //查看和审核
+        $scope.viewOrCheck = function(billNum){
+            $('#billNum').val(billNum);
+            $('#purchaseCheckModal').modal('show');
+        }
+
+        //审核结果拒绝理由输入框
+        $('.radioDiv input').click(function(){
+            if($(this).attr('checkValue') == 1){
+                $('.reasonDiv').hide();
+            }else{
+                $('.reasonDiv').show();
+            }
+        });
+
+        //确定审核
+        $scope.checkOk = function(){
+            var billNum,reason;
+            billNum = $('#billNum').val();
+            reason = $('#reasonText').is(':visible')?$('#reasonText').val():'';
+            console.log(reason);
+        }
     }]);
