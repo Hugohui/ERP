2bcc008d99922935d764b2460bb571306acf0dad
huiwenhua <huiwenhua@idriverplus.com>
2017-10-12 09:33:01 +0800

    采购申请审批，拒绝理由为空判断
    Change-Id: I76114bbc1b3cdfe8d1274421bc948eb450fd8eba

--- www/src/js/controllers/purchase/purchaseTrackController.js
+++ www/src/js/controllers/purchase/purchaseTrackController.js
@@ -19,8 +19,8 @@ mainStart
 
         loadPurchaseTrack();
 
-        //美化滚动条
-        $('.purchaseTrackBody').niceScroll({
+        //美化滚动条//页面跳转后任存在！！！
+/*        $('.purchaseTrackBody').niceScroll({
             cursorcolor: "#ccc",//#CC0071 光标颜色
             cursoropacitymax: 1, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0
             touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备
@@ -28,7 +28,7 @@ mainStart
             cursorborder: "0", // 游标边框css定义
             cursorborderradius: "5px",//以像素为光标边界半径
             autohidemode: false //是否隐藏滚动条
-        });
+        });*/
 
         $('.purchaseTrackBody').scroll(function(){
             stopLoadFlag = true;
