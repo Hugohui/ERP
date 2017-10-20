3b352a05b37c0b091dc2b503a43ddfa81a689e2c
huiwenhua <huiwenhua@idriverplus.com>
2017-10-20 18:11:18 +0800

    库存查询数据联调
    Change-Id: Ia9b8b7be9b1d66837e6851ca949519dc3eac07e2

--- www/src/js/controllers/depotManagement/depotRequestController.js
+++ www/src/js/controllers/depotManagement/depotRequestController.js
@@ -17,32 +17,6 @@ mainStart
 
         function initDepotRequestTable() {
             var scrollY = $('.mainView').height() - $('.queryDIv').height() - 120;
-            var lang = {
-                "sProcessing": "处理中...",
-                "sLengthMenu": "每页 _MENU_ 项",
-                "sZeroRecords": "没有匹配结果",
-                "sInfo": "当前显示第 _START_ 至 _END_ 项，共 _TOTAL_ 项。",
-                "sInfoEmpty": "当前显示第 0 至 0 项，共 0 项",
-                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
-                "sInfoPostFix": "",
-                "sSearch": "搜索:",
-                "sUrl": "",
-                "sEmptyTable": "表中数据为空",
-                "sLoadingRecords": "载入中...",
-                "sInfoThousands": ",",
-                "oPaginate": {
-                    "sFirst": "首页",
-                    "sPrevious": "上页",
-                    "sNext": "下页",
-                    "sLast": "末页",
-                    "sJump": "跳转"
-                },
-                "oAria": {
-                    "sSortAscending": ": 以升序排列此列",
-                    "sSortDescending": ": 以降序排列此列"
-                }
-            };
-
             //初始化表格
             depotRequestTable = $("#depotRequestTable").dataTable({
                 language: lang,  //提示信息
@@ -64,99 +38,93 @@ mainStart
                     }
                 ],
                 ajax: function (data, callback, settings) {
-                    //封装请求参数
                     var param = {};
-                    param.limit = data.length;//页面显示记录条数，在页面显示每页显示多少项的时候
-                    param.start = data.start;//开始的记录序号
-                    param.page = (data.start / data.length) + 1;//当前页码
+                    param.limit = data.length;
+                    param.start = data.start;
+                    param.page = (data.start / data.length) + 1;
                     param.applicant = $scope.user.name;
-                    //ajax请求数据
                     $.ajax({
                         type: 'POST',
-                        //url:'data/users.txt',
                         url: 'http://111.204.101.170:11115',
                         data: {
-                            action: "depotInputList",
+                            action: "depotRequest",
                             params: param
                         },
-                        //dataType:'json',
                         dataType: 'jsonp',
                         jsonp: "callback",
                         success: function (result) {
-                            //封装返回数据
                             var returnData = {};
-                            returnData.draw = data.draw;//这里直接自行返回了draw计数器,应该由后台返回
-                            returnData.recordsTotal = result.resData.total;//返回数据全部记录
-                            returnData.recordsFiltered = result.resData.total;//后台不实现过滤功能，每次查询均视作全部结果
-                            returnData.data = result.resData.data;//返回的数据列表
+                            returnData.draw = data.draw;
+                            returnData.recordsTotal = result.resData.total;
+                            returnData.recordsFiltered = result.resData.total;
+                            returnData.data = result.resData.data;
                             callback(returnData);
                         }
                     });
                 },
-                //列表表头字段
                 columns: [
                     {
-                        "data": "applicant",
+                        "data": "material_code",
                         "sClass": "text-center",
                     },
                     {
+                        "data": "material_name",
+                        "sClass": "text-center"
+                    },
+                    {
+                        "data": "model",
+                        "sClass": "text-center"
+                    },
+                    {
+                        "data": "sn_num",
+                        "sClass": "text-center"
+                    },
+                    {
+                        "data": "project_num",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "unit",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "stock_number",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "batch",
+                        "sClass": "text-center"
+                    },
+                    {
+                        "data": "brand",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "manufactor",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "unit_price",
+                        "sClass": "text-center"
+                    }
+                    ,
+                    {
+                        "data": "stock_position",
+                        "sClass": "text-center"
+                    }/*,
+                    {
                         "data": "applicant",
                         "sClass": "text-center"
                     },
                     {
                         "data": "applicant",
                         "sClass": "text-center"
-                    },
-                    {
-                        "data": "contract_num",
-                        "sClass": "text-center"
-                    },
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    },
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
-                    ,
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    },
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    },
-                    {
-                        "data": "applicant",
-                        "sClass": "text-center"
-                    }
+                    }*/
                 ]
             }).api();
         }
