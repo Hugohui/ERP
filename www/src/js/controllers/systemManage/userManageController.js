2b7966d1f9a775cfdf707a58907aafe885d3a739
huiwenhua <huiwenhua@idriverplus.com>
2017-10-30 10:18:53 +0800

    用户管理添加消息推行信息
    Change-Id: I96f4cf45ff9455860299fb070f79a18d8805c0c0

--- www/src/js/controllers/systemManage/userManageController.js
+++ www/src/js/controllers/systemManage/userManageController.js
@@ -5,6 +5,8 @@ mainStart
 
         //获取角色权限
         $scope.roles = $localStorage.roles;
+        //消息推送
+        $scope.sendMessage =JSON.parse(window.localStorage.getItem('ngStorage-sendMessage'));
         //获取角色信息
         $scope.user = $localStorage.user;
         //
