e5f01cb5b38c3e81f4766d4420ad0913617edfb3
huiwenhua <huiwenhua@idriverplus.com>
2017-11-03 11:14:00 +0800

    基础信息页面编写
    Change-Id: I23a261dacf578e6905dff9a9b470d04ea7e63cb2

--- www/src/js/controllers/purchase/basicInfoManageController.js
+++ www/src/js/controllers/purchase/basicInfoManageController.js
@@ -7,4 +7,32 @@ mainStart
         $scope.sendMessage = JSON.parse(window.localStorage.getItem('ngStorage-sendMessage'));
         //获取角色信息
         $scope.user = $localStorage.user;
+
+
+
+
+
+
+        //展开收起
+        $scope.expandOrPackUp = function(e){
+            var $this = $(e.target) ;
+            if($this.html() == '展开'){
+                $('.expand').html('展开');
+                $('.panelMain').removeClass('active');
+                $this.html('收起');
+                $this.closest('.panel').find('.panelMain').addClass('active');
+            }else{
+                $this.html('展开');
+                $this.closest('.panel').find('.panelMain').removeClass('active');
+            }
+        }
+
+/*        //清除已有的验证提示信息
+        $('#basicInfoManage [valType]').hideValidate();
+        //初始化验证
+        $.fn.InitValidator('basicInfoManage');
+
+        //提交前验证
+        var isValidate = beforeSubmit("basicInfoManage");*/
+
     }]);
